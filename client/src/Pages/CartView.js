02e11274b82faae32e22a72f9eae2cddc37f43e7
mrmaad <mohamedaadilrizam@gmail.com>
2020-05-30 23:25:15 +0530

    Add to cart function and mycartView

--- client/src/Pages/CartView.js
+++ client/src/Pages/CartView.js
@@ -16,20 +16,30 @@ import {useDispatch} from 'react-redux';
          this.state = {
              products: [] ,
              user:loginUser,
-             userID:''
+             userID:'',
+             myCart:[],
+             totQuantity:0
+
          };
         this.productList = this.productList.bind(this);
     }
 
-     addToCarthandler(productID){
-        console.log(productID);
-        const dispatch = useDispatch();
-        dispatch(addToCart(productID))
-     }
+    addToCarthandler = (product) => {
+        console.log('It from add to cart' + this.props.match.params.user)
+        this.setState({
+            userID: this.props.match.params.user,
+            myCart: [...this.state.myCart, product],
+            totQuantity: this.state.totQuantity + 1
+        }, () => {
+            console.log(this.state.userID)
+            console.log(this.state.myCart)
+            console.log(this.state.totQuantity)
+        })
+    }
 
 
     componentDidMount() {
-
+        this.setState({userID: this.props.match.params.user})
         console.log("This is user id :" +this.props.match.params.user);
 
         axios.get('http://localhost:4001/cart/')
@@ -52,7 +62,7 @@ import {useDispatch} from 'react-redux';
                         <p>{product.PDescription}</p>
                         <div className="clearfix">
                             <div className="pull-left" style={{fontWeight:"bold" ,fontSize:"16px"}}>Rs {product.PPrice}</div>
-                            <p className="pull-right"><a onClick={() => this.addToCarthandler(product._id)} className="btn btn-success" role="button">Add To Cart</a>
+                            <p className="pull-right"><a onClick={() => this.addToCarthandler(product)} className="btn btn-success" role="button">Add To Cart</a>
                                 <a href="#" className="btn btn-info" role="button">Wish List</a></p>
                         </div>
                     </div>
@@ -66,7 +76,7 @@ import {useDispatch} from 'react-redux';
 
         return(
             <div>
-                <LogedinHeader/>
+                <LogedinHeader myCart = {this.state.myCart} totQuantity = {this.state.totQuantity} userID = {this.state.userID}/>
                     <div className="container">
                         <div className="row">
                             {this.productList()}
